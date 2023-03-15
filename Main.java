03ab461fe106bccd3af69c347b5cddf4a842c432
isharatharindu <ishara2244tharidu@gmail.com>
2023-03-15 15:51:19 +0530

    create main

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
     Tree new1 = new Tree();
-    new1.Insert(200, "ishara");
-    new1.Insert(400, "isharatharindu");
-    new1.Insert(500, "isharatharfdindu");
-    new1.Insert(200, "isharatharindu");
-    new1.Insert(131, "isharatharfdindu");
+    new1.Insert(149, "Anushka");
+    new1.Insert(150, "Kosala");
+    new1.Insert(130, "dinusha");
+    new1.Insert(155, "Malith");
+    new1.Insert(131, "jayamini");
 
     new1.TraverseinOrder();
+    new1.Traverseperoder();
+    new1.Traversepostorder();
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Employee number :-");
+    int newone = sc.nextInt();
+
+      Node display =new1.callRecursive(newone);
+      display.DisplayNode();
+
+
+      new1.delerall();
    }   
 }
